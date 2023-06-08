39d33d5b6fdcbd1cfa04dc1a7ba2051d58839366
Gusarich <danielsedovzzz@gmail.com>
2023-06-08 18:25:13 +0300

    add env

--- backend/main.ts
+++ backend/main.ts
@@ -1,10 +1,14 @@
 import express from 'express';
 import { config } from 'dotenv';
+import { keyPairFromSecretKey } from 'ton-crypto';
+import { Address } from 'ton';
 
 config();
 
 const app = express();
 const port = process.env.PORT;
+const keyPair = keyPairFromSecretKey(Buffer.from(process.env.ADMIN_SECRET_KEY!, 'hex'));
+const adminAddress = Address.parse(process.env.ADMIN_ADDRESS!);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
