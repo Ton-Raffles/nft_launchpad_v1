1052f875ed364c388c3b33ccb7ab3fc99e3983cb
Gusarich <danielsedovzzz@gmail.com>
2023-06-10 11:35:13 +0300

    `checkIfAddressHoldsNFT` method

--- backend/main.ts
+++ backend/main.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import axios from 'axios';
 import { config } from 'dotenv';
 import { keyPairFromSecretKey } from 'ton-crypto';
 import { Address } from 'ton';
@@ -6,14 +7,37 @@ import { Address } from 'ton';
 config();
 
 const app = express();
-const port = process.env.PORT;
 const keyPair = keyPairFromSecretKey(Buffer.from(process.env.ADMIN_SECRET_KEY!, 'hex'));
 const adminAddress = Address.parse(process.env.ADMIN_ADDRESS!);
+const endpoint = process.env.TONAPI_ENDPOINT;
+const tonApiKey = process.env.TONAPI_KEY!;
 
-app.get('/', (req, res) => {
+async function checkIfAddressHoldsNFT(address: Address, collection: Address): Promise<boolean> {
+    const result = await axios.get(
+        endpoint +
+            '/v2/accounts/' +
+            address.toRawString() +
+            '/nfts?collection=' +
+            collection.toRawString() +
+            '&limit=1&indirect_ownership=false',
+        {
+            headers: {
+                Authorization: 'Bearer ' + tonApiKey,
+            },
+        }
+    );
+    const items = result.data.nft_items!;
+    return items.length > 0;
+}
+
+app.get('/', async (req, res) => {
     res.send('Hello World!');
+    checkIfAddressHoldsNFT(
+        Address.parse('EQBKgXCNLPexWhs2L79kiARR1phGH1LwXxRbNsCFF9doc2lN'),
+        Address.parse('EQDvRFMYLdxmvY3Tk-cfWMLqDnXF_EclO2Fp4wwj33WhlNFT')
+    );
 });
 
-app.listen(port, () => {
-    console.log(`API is listening on port ${port}`);
+app.listen(process.env.PORT!, () => {
+    console.log(`API is listening on port ${process.env.PORT!}`);
 });
